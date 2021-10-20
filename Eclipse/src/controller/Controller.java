c2791ceb108694c6047c7b8e1f78ab2e02876511
Brand28 <alejandrobrand28@gmail.com>
2021-10-19 23:07:23 -0500

    mas cositas iniciales

--- Eclipse/src/controller/Controller.java
+++ Eclipse/src/controller/Controller.java
@@ -1,5 +1,15 @@
 package controller;
 
+import model.Logic;
+import processing.core.PApplet;
+
 public class Controller {
+	
+	private PApplet app;
+	private Logic l;
+	
+	public Controller(PApplet app) {
+		this.app = app;
+	}
 
 }
--- Eclipse/src/view/Main.java
+++ Eclipse/src/view/Main.java
@@ -1,10 +1,27 @@
 package view;
 
-public class Main {
+import controller.Controller;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	
+	private Controller c;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		c = new Controller(this);
+	}
+	
+	public void draw() {
+		
 	}
 
 }
