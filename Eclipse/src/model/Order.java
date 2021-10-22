34714d65924f1732a199e292ca7c9452510ea71c
Brand28 <alejandrobrand28@gmail.com>
2021-10-21 21:57:40 -0500

    intentando separar ip y puertos recibidos

---
+++ Eclipse/src/model/Order.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Order {
+	
+	private PApplet app;
+	private PImage img;
+	private Date orderDate;
+}
--- Eclipse/src/view/UDPConnection.java
+++ Eclipse/src/view/UDPConnection.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class UDPConnection extends Thread{
 	
 	private DatagramSocket socket;
+	private String senderIP;
+	private int senderPort;
 	
 	public void run() {
 		try {
@@ -27,9 +30,12 @@ public class UDPConnection extends Thread{
 				System.out.println("Esperando Datagrama");
 				socket.receive(packet);
 				
-				
 				String message = new String(packet.getData()).trim();
 				System.out.println("Datagrama recibido " + message);
+				
+				String[] messageAddress = packet.getSocketAddress().toString().split(":");
+				senderIP = messageAddress[0].replace("/", "");
+				senderPort = Integer.parseInt(messageAddress[1]);
 			}
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
@@ -45,8 +51,8 @@ public class UDPConnection extends Thread{
 				()-> {
 					// necesita 4 parametros en constructor
 					try {
-						InetAddress	ip = InetAddress.getByName("127.0.0.1");
-						DatagramPacket packet = new DatagramPacket(message.getBytes(), message.getBytes().length, ip, 6000);
+						InetAddress	ip = InetAddress.getByName(senderIP);
+						DatagramPacket packet = new DatagramPacket(message.getBytes(), message.getBytes().length, ip, 5000);
 						socket.send(packet);
 					} catch (UnknownHostException e) {
 						// TODO Auto-generated catch block
